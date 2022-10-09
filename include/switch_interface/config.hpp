5283003226184236bf7598b823489ee344970073
Roman Kuzmenko <clairbee@guidemaze.com>
2022-10-09 00:09:23 -0700

    Added missed files

---
+++ include/switch_interface/config.hpp
@@ -0,0 +1,17 @@
+/*
+ * OpenVMP, 2022
+ *
+ * Author: Roman Kuzmenko
+ * Created: 2022-09-24
+ *
+ * Licensed under Apache License, Version 2.0.
+ */
+
+#ifndef OPENVMP_SWITCH_CONFIG_H
+#define OPENVMP_SWITCH_CONFIG_H
+
+#define SWITCH_CONFIG_TOP "switch"
+
+namespace switch_interface {}
+
+#endif  // OPENVMP_SWITCH_CONFIG_H
